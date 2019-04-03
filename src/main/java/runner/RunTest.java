628e59d1755b388295a68957780486e2dd81dd20
krishchaithu <chaithchaithu@gmail.com>
2019-04-03 16:35:23 +0530

    updated stepdefinations and feature files

--- src/main/java/runner/RunTest.java
+++ src/main/java/runner/RunTest.java
@@ -6,7 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="E://Learning//java//BddFramework//src//main//java//Features//login.feature", glue= {"StepDefination"}, format= {"pretty","html:test-output"}
+@CucumberOptions(features="E://Learning//java//BddFramework//src//main//java//Features//login.feature", glue= {"StepDefination"}, plugin= {"pretty","html:test-output"},monochrome=true,
+dryRun=true
 		)
 
 public class RunTest {
--- test-output/report.js
+++ test-output/report.js
@@ -8,9 +8,9 @@ formatter.feature({
 });
 formatter.scenario({
   "line": 3,
-  "name": "user able to login to akamai",
+  "name": "1 user able to login to a website with correct credentials",
   "description": "",
-  "id": "site-login-feature;user-able-to-login-to-akamai",
+  "id": "site-login-feature;1-user-able-to-login-to-a-website-with-correct-credentials",
   "type": "scenario",
   "keyword": "Scenario"
 });
@@ -53,49 +53,127 @@ formatter.match({
   "location": "LoginStepDefination.user_is_on_site_page()"
 });
 formatter.result({
-  "duration": 10018431265,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "LoginStepDefination.title_of_the_page_is_akamai()"
 });
 formatter.result({
-  "duration": 24844784,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "LoginStepDefination.user_enters_username()"
 });
 formatter.result({
-  "duration": 158375758,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "LoginStepDefination.user_enters_password()"
 });
 formatter.result({
-  "duration": 133723699,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "LoginStepDefination.user_click_on_submit_button()"
 });
 formatter.result({
-  "duration": 10881571130,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "LoginStepDefination.user_validates_title_page_is_Home()"
 });
 formatter.result({
-  "duration": 9056929,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "LoginStepDefination.close_browse()"
 });
 formatter.result({
-  "duration": 3213811722,
-  "status": "passed"
+  "status": "skipped"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "2 user not able to login to a website with incorrect credentials",
+  "description": "",
+  "id": "site-login-feature;2-user-not-able-to-login-to-a-website-with-incorrect-credentials",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 14,
+  "name": "user is on site page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 15,
+  "name": "title of the page is akamai",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 16,
+  "name": "user enters username",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 17,
+  "name": "user enters password",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "user click on submit button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 19,
+  "name": "user validates title page is Home",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 20,
+  "name": "close browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefination.user_is_on_site_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefination.title_of_the_page_is_akamai()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_enters_username()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_enters_password()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_click_on_submit_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefination.user_validates_title_page_is_Home()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefination.close_browse()"
+});
+formatter.result({
+  "status": "skipped"
 });
 });
